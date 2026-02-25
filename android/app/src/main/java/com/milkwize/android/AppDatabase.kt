8c0969973b385c82fc99a2bcd227b1514d10e2e1
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2026-02-25 22:17:29 +0300

    updated

--- android/app/src/main/java/com/milkwize/android/AppDatabase.kt
+++ android/app/src/main/java/com/milkwize/android/AppDatabase.kt
@@ -1,4 +1,23 @@
 package com.milkwize.android
 
-class AppDatabase {
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import android.content.Context
+
+@Database(entities = [LocalEvent::class], version = 1)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun milkingDao(): MilkingDao
+
+    companion object {
+        @Volatile private var instance: AppDatabase? = null
+        fun getDatabase(context: Context): AppDatabase {
+            return instance ?: synchronized(this) {
+                instance ?: Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java, "milkwize_db"
+                ).build().also { instance = it }
+            }
+        }
+    }
 }
