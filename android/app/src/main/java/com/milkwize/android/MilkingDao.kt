44f2614f4031a604e9ade49f750e969322b39f81
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2026-02-25 21:57:57 +0300

    updated

--- android/app/src/main/java/com/milkwize/android/MilkingDao.kt
+++ android/app/src/main/java/com/milkwize/android/MilkingDao.kt
@@ -1,4 +1,18 @@
 package com.milkwize.android
 
-class MilkingDao {
+import androidx.room.*
+
+@Dao
+interface MilkingDao {
+    @Insert
+    suspend fun insert(event: LocalEvent)
+
+    @Query("SELECT * FROM local_milking_events WHERE isSynced = 0")
+    suspend fun getUnsyncedEvents(): List<LocalEvent>
+
+    @Update
+    suspend fun update(event: LocalEvent)
+
+    @Query("SELECT * FROM local_milking_events ORDER BY id DESC")
+    fun getAllLocally(): kotlinx.coroutines.flow.Flow<List<LocalEvent>>
 }
