3825cb5115f68e679d3da6bc4a4c393db36535ff
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2026-03-03 00:00:01 +0300

    updated

--- android/app/src/main/java/com/milkwize/android/AppDatabase.kt
+++ android/app/src/main/java/com/milkwize/android/AppDatabase.kt
@@ -7,7 +7,7 @@ import android.content.Context
 
 @Database(entities = [LocalEvent::class], version = 1)
 abstract class AppDatabase : RoomDatabase() {
-    abstract fun (): MilkingDao
+    abstract fun milkingDao(): MilkingDao
 
     companion object {
         @Volatile private var instance: AppDatabase? = null
