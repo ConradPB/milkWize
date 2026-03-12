16760d1901b47666a6428181dde892b3c9b7b63a
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2026-03-12 16:38:42 +0300

    updated

--- android/app/src/main/java/com/milkwize/android/MilkingEvent.kt
+++ android/app/src/main/java/com/milkwize/android/MilkingEvent.kt
@@ -7,7 +7,11 @@ import kotlinx.serialization.SerialName
 data class MilkingEvent(
     val id: String? = null,
 
-    @SerialName("recorded_by") val recordedBy: String? = null,
+    @SerialName("owner_id")
+    val ownerId: String? = null,
+
+    @SerialName("recorded_by")
+    val recordedBy: String? = null,
 
     @SerialName("cow_id")
     val cowId: String,
