9f03073735075bbab3c5e7ca23b72c678d843321
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2026-03-15 17:18:42 +0300

    updated

--- android/app/src/main/java/com/milkwize/android/Models.kt
+++ android/app/src/main/java/com/milkwize/android/Models.kt
@@ -1,2 +1,29 @@
 package com.milkwize.android
 
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class UserProfile(
+    val id: String,
+    val email: String,
+    val role: String, // "farmer" or "customer"
+    val farm_code: String? = null
+)
+
+@Serializable
+data class Cow(
+    val id: String,
+    val ownerId: String,
+    val name: String,
+    val breed: String? = null
+)
+
+@Serializable
+data class MilkingEvent(
+    val id: Int? = null,
+    val cowId: String,
+    val ownerId: String,
+    val recordedBy: String,
+    val milkLiters: Double,
+    val milkingTime: String
+)
